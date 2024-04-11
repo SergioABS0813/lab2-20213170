4aad86569517098ebb38189213e36f7b5d76c88e
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-04-10 21:52:26 -0500

    commit-modificando.-url
    Se modifican las url

--- repasito/src/main/java/com/example/repasito/HomeController.java
+++ repasito/src/main/java/com/example/repasito/HomeController.java
@@ -3,8 +3,10 @@ package com.example.repasito;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @Controller
-@RequestMapping(value = {"/juego"})
+@RequestMapping(value = {""})
 public class HomeController {
 
     @GetMapping(value = {""})
@@ -13,13 +15,23 @@ public class HomeController {
     }
 
     @PostMapping(value = {"/imagenes"})
+    @ResponseBody
     public String metodo2(@RequestParam("numerofilas") int numFilas,
                           @RequestParam("numerocolumnas") int numColumnas,
                           @RequestParam("posicionesIniciales") String posicionesIniciales,
                           @RequestParam("numerofotos") int numeroFotos){
 
-        
-        return "imagenes";
+        String[] listaPosiciones = posicionesIniciales.split(" "); //separamos las posciones iniciales en una lista
+
+        //En base a numFilas y numColumnas generamos el espacio:
+
+        ArrayList<String> tablaTotal = new ArrayList<>();
+
+
+
+
+
+        return "se muestra la siguiente lista de posiciones:" + listaPosiciones[0];
     }
 
 }
