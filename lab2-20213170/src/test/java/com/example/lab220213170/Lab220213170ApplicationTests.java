6e52299cb3dbe350eefd4fb17817f78d4d2c6ace
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-04-10 21:06:28 -0500

    commit-despliegue
    despliegue del proyecto

---
+++ lab2-20213170/src/test/java/com/example/lab220213170/Lab220213170ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab220213170;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220213170ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
